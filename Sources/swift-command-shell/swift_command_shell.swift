e262c49711a51b5f808622e9b55ecc52f941d8c4
Anurag Ajwani <anurag.ajwani@travelperk.com>
2023-03-09 21:22:11 +0000

    Now executin shell commands and prnting output

--- Sources/swift-command-shell/swift_command_shell.swift
+++ Sources/swift-command-shell/swift_command_shell.swift
@@ -1,3 +1,4 @@
+import Foundation
 import ArgumentParser
 
 @main
@@ -6,6 +7,48 @@ struct swift_command_shell: ParsableCommand {
     private var shellCommand: String
 
     mutating func run() throws {
-        print("TODO execute \(shellCommand)")
+        let output = shell(shellCommand)
+        let jsonEncoder = JSONEncoder()
+        jsonEncoder.outputFormatting = .prettyPrinted
+        let data = try jsonEncoder.encode(output)
+        let shellOutputString = String(data: data, encoding: .utf8)!
+        print(shellOutputString)
     }
+}
+
+struct ShellOutput: Codable {
+    let command: String
+    let text: String?
+    let exitCode: Int32
+}
+
+@discardableResult
+func shell(_ command: String, printOutput: Bool = false) -> ShellOutput {
+    let task = Process()
+    let pipe = Pipe()
+
+    task.standardOutput = pipe
+    task.standardError = pipe
+    task.arguments = ["-c", command]
+    task.launchPath = "/bin/zsh"
+    task.standardInput = nil
+
+    if printOutput {
+        pipe.fileHandleForReading.readabilityHandler = { pipe in
+            let data = pipe.availableData
+            guard !data.isEmpty, let line = String(data: data, encoding: .utf8) else {
+                return
+            }
+            print(line.trimmingCharacters(in: .whitespacesAndNewlines))
+        }
+    }
+
+    task.launch()
+
+    let data = pipe.fileHandleForReading.readDataToEndOfFile()
+    let output = String(data: data, encoding: .utf8)
+
+    task.waitUntilExit()
+
+    return ShellOutput(command: command, text: output, exitCode: task.terminationStatus)
 }
